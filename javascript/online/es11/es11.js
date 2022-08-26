3ae910c49b439c66d709dcb42a26fa807c8c74ba
Simone Tassi <72198236+simonetassi@users.noreply.github.com>
2022-08-26 12:54:14 +0200

    added es11

---
+++ javascript/online/es11/es11.js
@@ -0,0 +1,35 @@
+function displayImage() {
+    var theImages = [{
+        src: "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg",
+        width: "240",
+        height: "160"
+    }, {
+        src: "http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg",
+        width: "320",
+        height: "195"
+    }, {
+        src: "http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg",
+        width: "500",
+        height: "343"
+    }];
+
+    var preBuffer = [];
+
+    for (var i = 0; i < theImages.length; i++) {
+        preBuffer[i] = new Image();
+        preBuffer[i].src = theImages[i].src;
+        preBuffer[i].width = theImages[i].width;
+        preBuffer[i].height = theImages[i].height;
+    }
+
+    rand = Math.floor(Math.random() * preBuffer.length);
+    var newImage = preBuffer[rand];
+
+    var images = document.getElementsByTagName('img');
+    var l = images.length;
+    for (var p = 0; p < l; p++) {
+        images[0].parentNode.removeChild(images[0]);
+    }
+
+    document.body.appendChild(newImage);
+}
