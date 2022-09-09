372d44d3827c711261ac8e8762eef430cb1a96a9
Simone Tassi <72198236+simonetassi@users.noreply.github.com>
2022-09-09 12:03:32 +0200

    finished js 20220623

--- prove/20220623/js/script.js
+++ prove/20220623/js/script.js
@@ -19,5 +19,100 @@ function populateEvents(events) {
 }
 
 function populateSeats(seats) {
+    var body = document.getElementById('seats');
+    var tbl = document.createElement('table');
+    var tbdy = document.createElement('tbody');
+    // head
+    var tr = document.createElement('tr');
+    for (var cell of Object.keys(val[0])) {
+        var td = document.createElement('th');
+        td.appendChild(document.createTextNode(cell))
+        tr.appendChild(td)
+    }
+    tbdy.appendChild(tr);
+    // body
+    for (var row of val) {
+        var tr = document.createElement('tr');
+        for (var cell of Object.values(row)) {
+            var td = document.createElement('td');
+            td.innerhtml = `<button id="seat-${i}-${j}">${cell}</button>`;
+            tr.appendChild(td)
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl);
+}
 
+// GETS
+
+function getShows(from, to) {
+    fetch(
+        `https://www.site.com/listing?from=${from}&to=${to}`,
+        { method: "GET" }
+    )
+        .then((response) => {
+            if (!response.ok) throw new Error(`Errore HTTP ${response.status}.`);
+            return response.json();
+        })
+        .then((json) => {
+            populateShows(json);
+        })
+        .catch((error) => {
+            throw new Error(error);
+        });
+}
+
+function getEvents(show) {
+    fetch(
+        `https://www.site.com/show?id=${show}`,
+        { method: "GET" }
+    )
+        .then((response) => {
+            if (!response.ok) throw new Error(`Errore HTTP ${response.status}`);
+            return response.json();
+        })
+        .then((json) => {
+            populateEvents(json);
+        })
+        .catch((error) => {
+            throw new Error(error);
+        })
+}
+
+// POSTS
+
+function singleBuy(seat) {
+    fetch(
+        `https://www.site.com/buy?place=${seat}`,
+        { method: "GET" }
+    )
+
+        .then((response) => {
+            if (!response.ok) throw new Error(`Errore HTTP ${response.status}`);
+            return response.json();
+        })
+        .then((json) => {
+            alert("Biglietto acquistato con successo!");
+        })
+        .catch((error) => {
+            throw new Error(error);
+        })
+}
+
+function multipleBuy(seats) {
+    fetch(
+        `www.site.com/multipleBuy?places=${seats}`,
+        { method: "POST" }
+    )
+        .then((response) => {
+            if (!response.ok) throw new Error(`Errore HTTP: ${response.status}`);
+            return response.json();
+        })
+        .then((json) => {
+            alert("Biglietti acquistati con successo!")
+        })
+        .catch((error) => {
+            throw new Error(error);
+        })
 }
